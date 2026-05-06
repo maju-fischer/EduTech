9e98850c3118c16fc6f600c9cfebc12b7e83e38a
maju-fischer <majuliafischer@gmail.com>
2026-05-06 00:01:41 -0300

    feat: criação da estrutura principal

--- src/org/example/Main.java
+++ src/org/example/Main.java
@@ -1,4 +1,42 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int opcao;
+        do {
+            System.out.println("\n==============================");
+            System.out.println("      BYTEBURGER SYSTEM");
+            System.out.println("==============================");
+            System.out.println("1- Novo Pedido");
+            System.out.println("2- Consultar Cardápio por Categoria");
+            System.out.println("3- Calcular Troco");
+            System.out.println("4- Sorteio do Dia");
+            System.out.println("0- Encerrar");
+            System.out.println("Selecione uma opção");
+
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+
+                case 2:
+
+                case 3:
+
+                case 4:
+
+                case 0:
+                    System.out.println("Encerrando o sistema...");
+                    break;
+            }
+
+        } while (opcao != 0);
+    }
 }
+
